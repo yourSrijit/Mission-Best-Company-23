f9a698d9c67a35958f2fdd1989a0b4d396f0d24e
Srijit Bera <berasrijit02@gmail.com>
2023-08-31 09:02:37 +0530

    Update Kth_smallest.java

--- Frequently asked prac/Kth_smallest.java
+++ Frequently asked prac/Kth_smallest.java
@@ -13,11 +13,11 @@ public class Kth_smallest {
         return pq.poll();
     }
     public static void main(String[] args) {
-        int arr[]={11,51,6,79,27,14,51,22,58,85,96,2,3,14,80,98,12};  //3th larg =85
+        int arr[]={11,51,6,79,27,14,51,22,58,85,96,2,3,14,80,98,12};  //4th small =11
 
         int n=4;
         int ans=findKthLargest(arr,n);
-        System.out.println(" so the "+n+" th largest element is "+ans);
+        System.out.println(" so the "+n+" th smallest element is "+ans);
     }
 }
 
