834df45054ce231ae178a35e6a4cd5e93db10513
Srijit Bera <berasrijit02@gmail.com>
2023-09-02 16:36:41 +0530

    Update Max_sum.java

--- Frequently asked prac/Max_sum.java
+++ Frequently asked prac/Max_sum.java
@@ -17,3 +17,19 @@ public class Max_sum {
         System.out.println("Max sum is "+findMaxSum(arr));
     }
 }
+
+
+*Leetcode*
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int curSum = nums[0];
+//         int maxSum = nums[0];
+
+//         for (int i = 1; i < nums.length; i++) {
+//             curSum = Math.max(nums[i], curSum + nums[i]);
+//             maxSum = Math.max(maxSum, curSum);
+//         }
+
+//         return maxSum;
+//     }
+// }
