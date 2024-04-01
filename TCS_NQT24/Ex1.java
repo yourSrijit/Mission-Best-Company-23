255e6bf5f564f8942d29dbae895957d87796ec79
yourSrijit <berasrijit02@gmail.com>
2024-04-02 00:14:01 +0530

    TCS NQT UPDATE PREVIOUS YEAR

---
+++ TCS_NQT24/Ex1.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Ex1 {
+  public static void main(String[] args) {
+  Scanner sc =new Scanner(System.in);
+    System.out.print("srijit");
+    int n=sc.nextInt();
+    int w=sc.nextInt();
+    if(w <2 && (w%2==0) && n >w ){
+      System.out.println("Invalid Input");
+     
+    }
+    else{
+      int tw=(4*n- w)/2;
+      System.out.println("TW= "+tw+" FW= "+(n-tw));
+    }
+      
+  }
+}
+
+/*abstractProblem Statement – An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). A company manager wants to make the production of both types of vehicle according to the given data below:
+
+  1st data, Total number of vehicle (two-wheeler + four-wheeler)=v
+  2nd data, Total number of wheels = W
+  The task is to find how many two-wheelers as well as four-wheelers need to manufacture as per the given data.
+  Example :
+
+  Input :
+  200  -> Value of V
+  540   -> Value of W
+
+  Output :
+  TW =130 FW=70
+
+  Explanation:
+  130+70 = 200 vehicles
+  (70*4)+(130*2)= 540 wheels
+
+  Constraints :
+
+  2<=W
+  W%2=0
+  V<W
+  Print “INVALID INPUT” , if inputs did not meet the constraints.
+  */
---
+++ TCS_NQT24/Ex4.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int row,col;
+  cin>>row>>col;
+  int arr[row][col];
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      cin>>arr[i][j];
+    }
+  }
+  int count,max=INT_MIN,index;
+  for(int i=0;i<row;i++){
+    count =0;
+    for(int j=0;j<col;j++){
+      if(arr[i][j]== 1){
+        count ++;
+      }
+      if(count > max){
+        max=count;
+        index=i;
+      }
+    }
+  }
+  cout <<"Max index is "<<(index+1);
+}
+
+
+
+/*
+A parking lot in a mall has RxC number of parking spaces. Each parking space will either be  empty(0) or full(1). The status (0/1) of a parking space is represented as the element of the matrix. The task is to find index of the prpeinzta row(R) in the parking lot that has the most of the parking spaces full(1).
+
+Note :
+RxC- Size of the matrix
+Elements of the matrix M should be only 0 or 1.
+
+Example 1:
+Input :
+3   -> Value of R(row)
+3    -> value of C(column)
+[0 1 0 1 1 0 1 1 1] -> Elements of the array M[R][C] where each element is separated by new line.
+Output :
+3  -> Row 3 has maximum number of 1’s
+
+Example 2:
+input :
+4 -> Value of R(row)
+3 -> Value of C(column)
+[0 1 0 1 1 0 1 0 1 1 1 1] -> Elements of the array M[R][C]
+Output :
+4  -> Row 4 has maximum number of 1’s*/
