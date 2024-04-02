374c77ed55c02b4385f557c04eeb8ffd1a025d3d
yourSrijit <berasrijit02@gmail.com>
2024-04-02 12:24:21 +0530

    Rotate array by k

--- TCS_NQT24/Digital/Q1_RotateArray.java
+++ TCS_NQT24/Digital/Q1_RotateArray.java
@@ -2,10 +2,31 @@ package TCS_NQT24.Digital;
 
 import java.util.Scanner;
 
-public class Q1 {
+public class Q1_RotateArray {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        int []temp=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
 
+
+        //Solution
+        for(int i=0;i<n;i++){
+            temp[(i+k)%n]=arr[i];
+        }
+
+        for(int i=0;i<n;i++){
+            arr[i]=temp[i];
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
 
