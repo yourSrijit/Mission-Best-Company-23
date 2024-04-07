27ecc71790d37cc47b1da609763d8e840281789c
yourSrijit <berasrijit02@gmail.com>
2024-04-07 11:22:09 +0530

    alculate the pairs count in the given array

--- TCS_NQT24/Ninja/Ex8_PairCount.java
+++ TCS_NQT24/Ninja/Ex8_PairCount.java
@@ -4,6 +4,8 @@ import  java.util.*;
 public class Ex8_PairCount {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        Map<Integer,Integer> map=new HashMap<>();
+
         int n= sc.nextInt();
         int arr[]=new int[n];
         for(int i=0;i<n;i++){
@@ -14,13 +16,48 @@ public class Ex8_PairCount {
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 if((arr[i]+arr[j])%2==0){
-                    count++;
+                    if(!map.containsKey(arr[i])){
+                        map.put(arr[i],arr[j]);
+                    }
+
                 }
             }
         }
         System.out.print(count);
     }
 }
+/*
+CPP Solution ✅✅✅✅✅✅✅
+#include <bits/stdc++.h>
+using namespace std;
+void countPair(int arr[],int n){
+    int count=0;
+    map<pair<int,int>,bool> map;
+    for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                pair<int,int> p=make_pair(arr[i],arr[j]);
+                if((arr[i]+arr[j])%2==0 && map[p]!=true){
+                   count++;
+                   map[p]=true;
+                }
+            }
+        }
+        cout<<count;
+}
+int main()
+{
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    countPair(arr,n);
+    return 0;
+}
+
+ */
 
 /*
 Write a program to calculate the pairs count in the given array such that the sum of the pairs is divisible by 2
