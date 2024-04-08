c367f7affddc0042857e36818c0484bfea318642
yourSrijit <berasrijit02@gmail.com>
2024-04-08 13:20:44 +0530

    Print the Union of two array

---
+++ TCS_NQT24/Ninja/Ex9_isGoodNumber.java
@@ -0,0 +1,20 @@
+package TCS_NQT24.Ninja;
+
+public class Ex9_isGoodNumber {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+You are playing a game in which you have to find the good number or bad number.
+A number is a good number if the number is divisible bt the sum of its digits
+Note: Print "Good Number" if number is a good number else print "Bad Number"
+
+Example 1:
+num=3
+output= Good Number
+
+Example 2:
+num=27
+output= Good Number
+ */
