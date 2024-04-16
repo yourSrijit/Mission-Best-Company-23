522c0b792c9e837cf73615e5580dbf18be2290ae
yourSrijit <berasrijit02@gmail.com>
2024-04-16 12:17:45 +0530

    Question Update

---
+++ TCS_NQT24/Digital/Q16_LargerSmallerNumber.java
@@ -0,0 +1,46 @@
+package TCS_NQT24.Digital;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q16_LargerSmallerNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+    }
+}
+
+/*
+Compute the nearest larger number by interchanging its digits updated.Given 2 numbers a and b find the smallest number greater than b by interchanging the digits of a and if not possible print -1.
+
+Input Format
+2 numbers a and b, separated by space.
+Output Format
+A single number greater than b.
+If not possible, print -1
+
+Constraints
+1 <= a,b <= 10000000
+
+Example 1:
+
+Sample Input:
+
+    459 500
+
+Sample Output:
+    549
+
+Example 2:
+
+Sample Input:
+
+    645757 457765
+
+Sample Output:
+    465577
+ */
