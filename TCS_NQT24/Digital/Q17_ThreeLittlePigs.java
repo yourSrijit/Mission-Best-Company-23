ab671d5ca3d1ea629d854b1cf0aea4bd6d448682
yourSrijit <berasrijit02@gmail.com>
2024-04-18 22:02:26 +0530

    Question Update

---
+++ TCS_NQT24/Digital/Q17_ThreeLittlePigs.java
@@ -0,0 +1,17 @@
+package TCS_NQT24.Digital;
+
+public class Q17_ThreeLittlePigs {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+Three Little Pigs
+Three little pigs want to build houses along the river. There are many apple trees along the river. Each tree has its own quality level which affects the overall quality of the garden. A positive value means that the tree is good. A zero or negative value means that the tree is bad.
+The little pigs want to divide the territory in such a way that each of them gets gardens of the same quality. They need your help to determine how many ways they can divide the territory.
+Input
+The first line of input contains an integer N, representing the number of apple trees along the river.
+The second line of input contains N space separated integers as a1, a2... a, where a, represents the quality of ith tree.
+Output
+Print the number of ways the three little pigs can divide the territory.
+ */
---
+++ TCS_NQT24/Digital/Q_18_SingleDigitSum.java
@@ -0,0 +1,59 @@
+package TCS_NQT24.Digital;
+
+import java.util.*;
+
+public class Q_18_SingleDigitSum {
+    public static int Sum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static int single(int n) {
+        while (n > 9) {
+            n = Sum(n);
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int R = sc.nextInt();
+        if (R == 0) {
+            System.out.println(0);
+            return;
+        } else {
+            int sum = Sum(N);
+            int p = sum * R;
+            int ans = single(p);
+            System.out.println(ans);
+        }
+
+    }
+}
+
+/*
+An intelligence agency has received reports about some threats. The reports consist of numbers in a mysterious
+method. There is a number “N” and another number “R”. Those numbers are studied thoroughly and it is concluded
+that all digits of the number ‘N’ are summed up and this action is performed ‘R’ number of times. The resultant is also
+a single digit that is yet to be deciphered. The task here is to find the single-digit sum of the given number ‘N’ by
+repeating the action ‘R’ number of times.
+If the value of ‘R’ is 0, print the output as ‘0’.
+Example 1:
+Input :
+99 -> Value of N
+3 -> Value of R
+Output :
+9 -> Possible ways to fill the cistern.
+Explanation:
+Here, the number N=99
+1. Sum of the digits N: 9+9 = 18
+2. Repeat step 2 ‘R’ times i.e. 3 times (9+9)+(9+9)+(9+9) = 18+18+18 =54
+3. Add digits of 54 as we need a single digit 5+4
+Hence , the output is 9.
+
+ */
