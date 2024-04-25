92896330d257140d86764cb27a8e08bbd0f939a0
yourSrijit <berasrijit02@gmail.com>
2024-04-25 23:09:03 +0530

    Longest Substring with no repeating

--- TCS_NQT24/Digital/Q10_LongestSubstringWithoutRepeatingChar.java
+++ TCS_NQT24/Digital/Q10_LongestSubstringWithoutRepeatingChar.java
@@ -40,3 +40,42 @@ input : s="abcabcbb"
 output=3
 Explanation: The answer is 'abc' with the length of 3
  */
+
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    map<char,int> map;
+    int i=0,j=0;
+    int ans=INT_MIN;
+
+    while(j < s.size())
+    {
+        map[s[j]]++;
+
+        if(map.size()==j-i+1){
+            ans=max(ans,j-i+1);
+        }
+        else if(map.size() < j-i+1){
+            while(map.size() < j-i+1){
+                map[s[i]]--;
+                if(map[s[i]] ==0){
+                    map.erase(s[i]);
+                }
+                i++;
+            }
+        }
+        j++;
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
+
+ */
